67c3eebb504156f6a0f2c64c3a9519f49914cbdb
powerpook <powerkey@ibax.io>
2020-07-17 19:13:48 +0800

    return update

--- cmd/version.go
+++ cmd/version.go
@@ -7,9 +7,9 @@ package cmd
 
 import (
 	"fmt"
-// versionCmd represents the version command
-var versionCmd = &cobra.Command{
-	Use:   "version",
+
+	"github.com/IBAX-io/go-ibax/packages/consts"
+
 	Short: "Show version",
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println(consts.Version())
