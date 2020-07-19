0a0966fd4a07fbf458533fab5d7f1a86a48fb364
powerpook <powerkey@ibax.io>
2020-07-19 19:10:10 +0800

    full_rate

---
+++ packages/system/system_notwindows.go
@@ -0,0 +1,8 @@
+// +build !windows
+
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) IBAX. All rights reserved.
+ *  See LICENSE in the project root for license information.
+
+func killChildProc() {
+}
