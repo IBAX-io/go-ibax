b0b8a78fb0a2054706e82e0abdb3c835b3f74b41
powerpook <powerkey@ibax.io>
2020-08-29 10:08:02 +0800

    added sysrollback,removed price function and CallContract flags

--- packages/api/mintcount_test.go
+++ packages/api/mintcount_test.go
@@ -1,11 +1,15 @@
 /*---------------------------------------------------------------------------------------------
  *  Copyright (c) IBAX. All rights reserved.
+ *  See LICENSE in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+package api
+
+import (
+	"github.com/IBAX-io/go-ibax/packages/model"
+	"testing"
+)
+
 func TestMineCount(t *testing.T) {
 	var ret model.Response
 	err := sendGet(`mintcount/163`, nil, &ret)
 	if err != nil {
-		t.Error(err)
-		return
-	}
-
-}
