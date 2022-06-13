76a89bd3806e11c19acf38aa46a3c05407036726
powerpook <powerkey@ibax.io>
2022-06-13 14:31:49 +0800

    add SpentInfo struct

---
+++ packages/storage/sqldb/spent_info.go
@@ -0,0 +1,58 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) IBAX. All rights reserved.
+ *  See LICENSE in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+package sqldb
+
+import "gorm.io/gorm/clause"
+
+// SpentInfo is model
+type SpentInfo struct {
+	InputTxHash  string
+	InputIndex   int32
+	OutputTxHash string `gorm:"not null"`
+	OutputIndex  int32  `gorm:"not null"`
+	OutputKeyId  int64  `gorm:"not null"`
+	OutputValue  string `gorm:"not null"`
+	Scene        string
+	Ecosystem    int64
+	Contract     string
+	BlockId      int64
+	Asset        string
+}
+
+// TableName returns name of table
+func (si *SpentInfo) TableName() string {
+	return "spent_info"
+}
+
+// CreateSpentInfoBatches is creating record of model
+func CreateSpentInfoBatches(db *DbTransaction, spentInfos []*SpentInfo) error {
+	return GetDB(db).Debug().Clauses(clause.OnConflict{
+		Columns:   []clause.Column{{Name: "output_tx_hash"}, {Name: "output_key_id"}, {Name: "output_index"}},
+		DoUpdates: clause.AssignmentColumns([]string{"input_tx_hash", "input_index"}),
+		Where: clause.Where{Exprs: []clause.Expression{
+			clause.Eq{Column: "spent_info.output_tx_hash", Value: "EXCLUDED.output_tx_hash"},
+			clause.Eq{Column: "spent_info.output_key_id", Value: "EXCLUDED.output_key_id"},
+			clause.Eq{Column: "spent_info.output_index", Value: "EXCLUDED.output_index"},
+		}},
+	}).CreateInBatches(&spentInfos, 5000).Error
+}
+
+func GetTxOutputs(db *DbTransaction, keyIds []int64) ([]*SpentInfo, error) {
+	query :=
+		` SELECT
+			si.input_tx_hash,row_number() over (PARTITION BY si.output_key_id order by si.block_id ASC, tr.timestamp ASC) AS input_index,
+			si.output_tx_hash, si.output_index, si.output_key_id, si.output_value, si.scene, si.ecosystem, si.contract, si.block, si.asset
+		FROM
+			spent_info si LEFT JOIN log_transactions AS tr ON si.output_tx_hash = tr.hash
+		WHERE output_key_id IN ? AND si.input_tx_hash IS NULL
+		ORDER BY si.output_key_id, si.block_id ASC, tr.timestamp ASC `
+	var result []*SpentInfo
+	err := GetDB(db).Debug().Raw(query, keyIds).Scan(&result).Error
+	if err != nil {
+		return nil, err
+	}
+	return result, nil
+}
