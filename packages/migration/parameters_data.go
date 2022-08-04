3d18d25f3f9c77666d5d68de7ed8e7710146ff66
scottafk <scott@ibax.io>
2022-08-04 18:54:29 +0800

    change perm to dev

--- packages/migration/parameters_data.go
+++ packages/migration/parameters_data.go
@@ -16,11 +16,11 @@ INSERT INTO "1_parameters" ("id","name", "value", "conditions", "ecosystem") VAL
 	(next_id('1_parameters'),'changing_contracts', 'ContractConditions("DeveloperCondition")', 'ContractConditions("DeveloperCondition")', '{{.Ecosystem}}'),
 	(next_id('1_parameters'),'changing_parameters', 'ContractConditions("DeveloperCondition")', 'ContractConditions("DeveloperCondition")', '{{.Ecosystem}}'),
 	(next_id('1_parameters'),'changing_app_params', 'ContractConditions("DeveloperCondition")', 'ContractConditions("DeveloperCondition")', '{{.Ecosystem}}'),
+	(next_id('1_parameters'),'changing_snippets', 'ContractConditions("DeveloperCondition")', 'ContractConditions("DeveloperCondition")', '{{.Ecosystem}}'),
 	(next_id('1_parameters'),'max_sum', '1000000', 'ContractConditions("DeveloperCondition")', '{{.Ecosystem}}'),
 	(next_id('1_parameters'),'print_stylesheet', 'body {
 		  /* You can define your custom styles here or create custom CSS rules */
 	}', 'ContractConditions("DeveloperCondition")', '{{.Ecosystem}}'),
 	(next_id('1_parameters'),'min_page_validate_count', '1', 'ContractConditions("DeveloperCondition")', '{{.Ecosystem}}'),
-	(next_id('1_parameters'),'max_page_validate_count', '6', 'ContractConditions("DeveloperCondition")', '{{.Ecosystem}}'),
-	(next_id('1_parameters'),'changing_snippets', 'ContractConditions("MainCondition")', 'ContractConditions("DeveloperCondition")', '{{.Ecosystem}}');
+	(next_id('1_parameters'),'max_page_validate_count', '6', 'ContractConditions("DeveloperCondition")', '{{.Ecosystem}}');
 `
