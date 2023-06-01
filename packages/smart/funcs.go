93abb4d914edc2f32aa8f533202141e882b1811f
powerpook <powerkey@ibax.io>
2023-06-01 18:16:34 +0800

    add funcs TronAddress

--- packages/smart/funcs.go
+++ packages/smart/funcs.go
@@ -217,6 +217,7 @@ func EmbedFuncs(vt script.VMType) map[string]any {
 		"Replace":                      Replace,
 		"Size":                         Size,
 		"PubToID":                      PubToID,
+		"SeedToID":                     crypto.AddressSeed,
 		"HexToBytes":                   HexToBytes,
 		"LangRes":                      LangRes,
 		"HasPrefix":                    strings.HasPrefix,
@@ -247,6 +248,7 @@ func EmbedFuncs(vt script.VMType) map[string]any {
 		"SortedKeys":                   SortedKeys,
 		"Append":                       Append,
 		"EthereumAddress":              EthereumAddress,
+		"TronAddress":                  TronAddress,
 		"GetLogTxCount":                GetLogTxCount,
 		"GetHistory":                   GetHistory,
 		"GetHistoryRow":                GetHistoryRow,
@@ -2029,6 +2031,13 @@ func EthereumAddress(pub []byte) string {
 	keccak256 := &hashalgo.Keccak256{}
 	copy(addr[:], keccak256.GetHash(pub[:])[12:])
 	return addr.Hex()
+}
+
+func TronAddress(pub []byte) (string, error) {
+	keccak256 := &hashalgo.Keccak256{}
+	hash := keccak256.GetHash(pub[:])
+	hex20 := "41" + hex.EncodeToString(hash[len(hash)-20:])
+	return crypto.FromHexAddress(hex20)
 }
 
 func GetLogTxCount(sc *SmartContract, ecosystemID int64) (int64, error) {
